9529c4d9863779bec8cf7c0b159634d52982daa0
l0gesh29 <logeshperiyasamy24@gmail.com>
2020-10-27 20:10:45 +0530

    basic arithmetic operation of python

---
+++ ArithmeticOperator/ArithmeticOperator/Mul.py
@@ -0,0 +1,21 @@
+class Overload:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    def __mul__(self, other):
+        s = Overload(0, 0)
+        s.a = self.a * other.a
+        s.b = self.b * other.b
+        return s
+     
+    def display(self):
+        print("Value of a = ", self.a, "  Value of b = ", self.b)
+        return 
+     
+    pass
+
+p1 = Overload(5, 20)
+p2 = Overload(20, 4)
+p3 = p1*p2
+p3.display()
